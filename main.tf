d2809facfe2b4edc47e9250deda1f2e2dd5dfc8f
Balaji-Balaraman <148526712+Balaji-Balaraman@users.noreply.github.com>
2025-06-18 12:11:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,20 +4,40 @@ provider "aws" {
   secret_key = "RdZiYT/9loWPoaGNoT6mqE9SZUaBfXQK1/JKTFy6"
 }
 
+# Generate random ID for S3 bucket name uniqueness
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
 
 # Create S3 Bucket
 resource "aws_s3_bucket" "reports" {
-  bucket = "reports-${random_id.bucket_id.hex}"
+  bucket        = "reports-${random_id.bucket_id.hex}"
   force_destroy = true
-}
-
-resource "random_id" "bucket_id" {
-  byte_length = 4
 }
 
 # Create ECR Repository
 resource "aws_ecr_repository" "appimages" {
   name = "appimages"
+}
+
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get a default subnet (workaround for old Terraform)
+data "aws_subnet" "default" {
+  filter {
+    name   = "default-for-az"
+    values = ["true"]
+  }
+
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+
+  availability_zone = "us-east-1a" # adjust if needed
 }
 
 # Create Security Group to allow all ports
@@ -41,37 +61,7 @@ resource "aws_security_group" "allow_all" {
   }
 }
 
-# Get default VPC
-data "aws_vpc" "default" {
-  default = true
-}
-
-# Get default subnet
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
-}
-
-# Create EC2 instance with Ubuntu
-resource "aws_instance" "devops" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.large" # 8GB RAM
-
-  subnet_id              = data.aws_subnet_ids.default.ids[0]
-  security_groups        = [aws_security_group.allow_all.name]
-  associate_public_ip_address = true
-  key_name               = "mykey"
-
-  root_block_device {
-    volume_size = 75
-    volume_type = "gp2"
-  }
-
-  tags = {
-    Name = "Devops"
-  }
-}
-
-# Get Ubuntu AMI
+# Get latest Ubuntu AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -87,3 +77,24 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
+
+# EC2 Instance
+resource "aws_instance" "devops" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.large" # 8GB RAM
+  subnet_id              = data.aws_subnet.default.id
+  security_groups        = [aws_security_group.allow_all.name]
+  associate_public_ip_address = true
+  key_name               = "mykey" # must already exist in AWS
+
+  root_block_device {
+    volume_size = 75
+    volume_type = "gp2"
+  }
+
+  tags = {
+    Name = "Devops"
+  }
+}
+
+# No input vars needed since we're hardcoding everything for now
